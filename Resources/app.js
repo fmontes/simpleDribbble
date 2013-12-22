8619d8317c2b6f400b2fde4c8056989995eea587
Freddy Montes <fmontes@backcountry.com>
2013-12-22 08:13:47 -0600

    Updating README file

--- Resources/app.js
+++ Resources/app.js
@@ -21,7 +21,7 @@ myRequest.send();
 
 // Creatint the layout
 var nav = Ti.UI.createWindow({
-  backgroundColor: '#eee',
+  backgroundColor: '#eee'
 });
 
 var content = Ti.UI.createWindow({
@@ -47,14 +47,74 @@ var scroll = Ti.UI.createScrollView({
 nav.add(scroll);
 
 function loadThumbnails() {
-  for (var i = 0; i < shots.lenght; i++) {
+  for (var i = 0; i < shots.length; i++) {
     var thumb = Ti.UI.createImageView({
         image: shots[i].image_teaser_url,
-        lageImage: shots[i].image_url,
+        largeImage: shots[i].image_url,
         player: shots[i].player,
         height: 150,
-        top: 1 * 170,
+        top: i * 170,
     });
+    thumb.addEventListener('touchstart', function(e) {
+      mainImage.image = e.source.largeImage;
+      popover.title = e.source.player.name;
+      profilePic.image = e.source.player.avatar_url;
+      twitterName.text = e.source.player.twitter_screen_name;
+      location.text = e.source.player.location;
+    })
     scroll.add(thumb);
   }
 }
+
+
+
+// Creating main image
+var mainImage = Ti.UI.createImageView({
+  width: 400,
+  height: 300,
+});
+content.add(mainImage);
+
+
+
+
+// Adding the popover
+var popover = Ti.UI.iPad.createPopover({
+  width: 250,
+  height: 110,
+  arrowDirection: Ti.UI.iPad.POPOVER_ARROW_DIRECTION_DOWN
+});
+
+mainImage.addEventListener('touchstart', function(e) {
+  popover.show({
+    view: mainImage
+  })
+})
+
+var profilePic = Ti.UI.createImageView({
+  width: 80,
+  height: 80,
+  left: 0,
+})
+
+var twitterName = Ti.UI.createLabel({
+  width: 140,
+  left: 120,
+  color: '#fff',
+  font: {fontSize: 16},
+  top: 30,
+  height: 30
+})
+
+var location = Ti.UI.createLabel({
+  color: '#fff',
+  font: {fontSize:16},
+  left: 120,
+  width: 140,
+  top: 60,
+  height: 30
+})
+
+popover.add(profilePic);
+popover.add(twitterName);
+popover.add(location);
